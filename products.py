40a112637535297809b1346ac431eb093f08f6ee
kevin Chen <kevinx350@gmail.com>
2019-09-19 16:15:39 +0800

    商品名稱價格練習

---
+++ products.py
@@ -0,0 +1,10 @@
+products = []
+while True:
+	name = input("商品名稱:")
+	if name == 'q':
+		break
+	price = input('商品價格：')
+	products.append([name,price])
+print(products)	
+for p in products:
+	print(p)
